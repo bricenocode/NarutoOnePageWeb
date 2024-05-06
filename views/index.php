71a5d98c182e80a81d077902d74e9cfb09f6cc61
Miguel David Briceño Goncalves <miguelbricenocode@gmail.com>
2024-05-06 04:02:13 +0200

    Insert de todos los archivos
    Aqui estoy introduciendo todos los archivos que llevaba hechos que conformaran la V1.0 del proyecto

---
+++ views/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Naruto Shippuden</title>
+    <link rel="stylesheet" href="../assets/css/styles.css">
+</head>
+<body>
+
+    <!--Seccion del menu de navegacion-->
+    
+    <nav class="nav" id="nav">
+        <ul class="nav nav__list">
+            <li class="nav__item"><a href="#main-header" class="nav__link">INICIO</a></li>
+            <li class="nav__item"><a href="#personajesPrincipales" class="nav__link">PERSONAJES PRINCIPALES</a></li>
+            <li class="nav__item"><a href="#historia" class="nav__link">HISTORIA</a></li>
+            <li class="nav__item"><a href="#jutsustecnicas" class="nav__link">JUTSUS Y TÉCNICAS</a></li>
+            <li class="nav__item"><a href="#aldealocalizacion" class="nav__link">ALDEAS Y LOCALIZACIONES</a></li>
+            <li class="nav__item"><a href="#curiosidades" class="nav__link">CURIOSIDADES</a></li>
+            <li class="nav__item"><a href="#galeria" class="nav__link">GALERIA</a></li>
+            <li class="nav__item"><a href="#aboutus" class="nav__link">ABOUT US</a></li>
+        </ul>
+    </nav>
+    <!--Seccion de bienvenida a la página-->
+
+    <header class="main-header" id="main-header">
+        <div class="container">
+            <img src="https://i.ebayimg.com/images/g/C4QAAOSwrPJhsUos/s-l1600.png" alt="Logo de naruto" class="main-header logo">
+            <div class="main-header box">
+                <img src="https://www.pngall.com/wp-content/uploads/13/Naruto-Uzumaki-PNG-Cutout.png" alt="imagen de naruto de pie modo sobrado" class="box--img" alt="naruto re piola"> 
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates error rem accusamus, qui nam non quia, fuga nesciunt omnis eaque mollitia. Sunt earum dignissimos hic saepe aliquam suscipit ipsa aspernatur.</p>
+                <a href="#nav" class="boton-volver">Volver al inicio</a>
+            </div>  
+        </div>
+    </header> 
+
+    <!--Seccion de personajes principales-->
+
+    <section id="personajesPrincipales">
+        <div class="container">
+            <h2>PERSONAJES PRINCIPALES</h2>
+            </p>
+            <div class="personajesPrincipales--column">
+                <div class="personajesPrincipales--box">
+                    
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum expedita laudantium ex libero voluptas sint quae esse! Voluptatum, qui repudiandae. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni impedit deserunt molestiae harum enim similique, consequatur hic provident, veniam adipisci assumenda aliquid nam aut aspernatur sequi blanditiis nemo, necessitatibus debitis. </p>
+                    <img src="https://i.pinimg.com/originals/d3/46/91/d346917deed14e8075f531c9d8c46ab7.png" alt="" class="imagenNaruto" alt="imagen pepona" title="Naruto Uzumaki">
+                    <div class="flexbox">
+                        <div class="flex-item"><img src="../assets/img//INCENDIO.png" alt="" class="icon-element" title="INCENDIO"></div>
+                        <div class="flex-item"><img src="../assets/img/VIENTO.png" alt="viento" class="icon-element" title="VIENTO"></div>
+                        <div class="flex-item"><img src="../assets/img/yang.png" alt="" class="icon-element" title="YANG"></div>
+                        <div class="flex-item"><img src="../assets/img/TIERRA.png" alt="" class="icon-element" title="TIERRA"></div>
+                        <div class="flex-item"><img src="../assets/img/AGUA.png" alt="" class="icon-element" title="AGUA"></div>
+                        <div class="flex-item"><img src="../assets/img/FUEGO.png" alt="" class="icon-element" title="FUEGO"></div>
+                    </div>
+                </div>
+                <div class="personajesPrincipales--box">
+                    <img src="https://i.pinimg.com/originals/5b/91/42/5b9142c911364df6967253caf666ed7e.png" alt="" class="imagenSasuke" alt="imagen pepona" title="Sasuke Uchiha">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum expedita laudantium ex libero voluptas sint quae esse! Voluptatum, qui repudiandae.</p>
+                    <div class="flexbox">
+                        <div class="flex-item"><img src="../assets/img/yang.png" alt="" class="icon-element" title="YANG"></div>
+                        <div class="flex-item"><img src="../assets/img/RAYO.png" alt="" class="icon-element" title="RAYO"></div>
+                        <div class="flex-item"><img src="../assets/img/FUEGO.png" alt="" class="icon-element" title="FUEGO"></div>
+                    </div>
+                </div>
+                <div class="personajesPrincipales--box">
+                    <img src="https://i.pinimg.com/originals/cb/47/a2/cb47a2d3c62f3d40ec7239fbf2a7891c.png" alt="" class="imagenSakura" alt="imagen pepona" title="Sakura Haruno">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum expedita laudantium ex libero voluptas sint quae esse! Voluptatum, qui repudiandae.</p>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+        <!--Seccion de historia-->
+
+    <section id="historia">
+        <div class="historia background">
+            <div class="container">
+                <h2>HISTORIA</h2>
+                <p class="info-history" id="info-history">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quaerat culpa, quia nisi delectus iure provident exercitationem sed animi eos incidunt nulla nam quo voluptatibus dignissimos consectetur! Eius inventore blanditiis similique at necessitatibus pariatur alias dignissimos aut commodi soluta voluptatum ex obcaecati accusantium facilis ullam sequi praesentium quia ipsum, illum natus. Numquam pariatur consectetur inventore dolore! Debitis nobis quos consequuntur dolore neque soluta dolores repudiandae! Dolore error ipsam optio expedita dicta mollitia explicabo odit vel voluptatum recusandae similique, quasi laborum esse corporis doloribus. Corporis illo voluptates blanditiis, incidunt iusto quia quidem dolore eaque culpa sit quaerat aut reprehenderit ut, error quisquam dicta nobis eum, recusandae suscipit est dolorum velit debitis. Quam adipisci vero accusantium incidunt quidem fugiat animi, ut nobis quisquam numquam ipsum deserunt veniam dolores minima ratione tempore dolore sapiente iusto ex doloribus inventore. Doloribus vero cumque magnam voluptas itaque quaerat dolores quasi quam voluptate! Nostrum perspiciatis, eos voluptate tempora nemo sed aperiam tempore iste nesciunt quas aliquam consequatur facere adipisci suscipit odio aut, vitae illo ea quaerat eaque temporibus voluptates dolor magni reiciendis! Sed error commodi, dignissimos iste quis, eaque obcaecati alias animi ex quas quo. Optio a beatae, iste adipisci eligendi accusamus assumenda ut! Distinctio deserunt earum inventore incidunt quia. Tenetur est commodi ullam quo, praesentium eos obcaecati optio cum possimus distinctio quibusdam nemo sapiente ducimus expedita sunt eius excepturi autem, error porro ab dolore hic? Temporibus consectetur possimus sapiente nobis neque odit, commodi quas, veritatis inventore, amet necessitatibus deserunt facere quaerat laboriosam dolorem reprehenderit impedit doloribus? Impedit, nisi ipsa! Ea consequatur iure, ex deserunt laboriosam reprehenderit perspiciatis eius neque repudiandae, iste corrupti animi quaerat voluptates asperiores aut autem. Ipsum ullam reiciendis cupiditate voluptate, illo, tempore maiores, fuga dolorem quia laudantium rerum laboriosam nulla! Iure, provident sed. Repellat ad vero sint, beatae temporibus ullam cupiditate omnis sit. At architecto sint doloremque molestiae in eveniet consectetur omnis voluptate quibusdam quasi, veritatis, quaerat officiis recusandae voluptatem et, eligendi laboriosam necessitatibus! Quod, porro consequatur eaque aperiam ipsum laudantium eos in tenetur nemo aut nostrum fugiat saepe natus magni quas quos odit ducimus? Deserunt quibusdam vitae sequi eos pariatur qui. Error quasi, nisi corporis sint ullam itaque accusamus sequi? Nemo doloremque fugit sint labore soluta delectus dolores laudantium recusandae quidem. Non eum laborum perspiciatis itaque ratione ab voluptas qui sequi in doloremque officiis voluptatibus quibusdam eos quas eligendi quasi cupiditate nobis est illo quis, id omnis impedit quod ut. Similique quas distinctio magni labore ratione nam delectus consequuntur laudantium enim libero minus illo iusto officiis voluptatem dolores mollitia odio inventore doloribus, cumque cum. Quos, ad architecto! Fuga modi facilis ullam obcaecati. Vero neque eaque, esse temporibus doloremque itaque quos debitis consequatur, perspiciatis explicabo perferendis similique assumenda sit. Quia earum tempore dignissimos. Eveniet illo numquam, animi adipisci nihil incidunt quae rerum consectetur nesciunt rem, ut tempora magni repellendus cumque eligendi, architecto recusandae aut? Nemo quisquam, maiores quibusdam quas iusto libero ad facere, omnis soluta ex numquam! Accusamus veniam minus incidunt nihil quia saepe. Repellat, suscipit aliquam. Maiores, ipsam. Illum veniam eveniet ad, quasi totam maxime laboriosam iusto ab. Suscipit asperiores quis voluptas, cumque natus et itaque. Consequatur reprehenderit molestias porro nulla, veritatis error id eius. Quisquam officia inventore odio quasi maxime excepturi earum facilis molestias, tenetur asperiores voluptatem iste ullam, eaque obcaecati blanditiis ut voluptatum doloremque nisi, eligendi non. Incidunt architecto, minus reiciendis earum ipsum adipisci id quisquam dolorum! Laudantium, at, minima quaerat asperiores odit commodi perferendis sunt sint rerum blanditiis nihil fugiat voluptatum excepturi nam veniam vel. Ab quos incidunt deleniti minus corrupti ipsam eaque, veritatis repudiandae quaerat odio ad excepturi dolorem. Amet voluptates tempora adipisci quidem nesciunt, nisi quo perspiciatis quibusdam cum provident quas labore exercitationem, architecto vitae, nulla vel numquam expedita ut accusamus incidunt cumque deleniti? Provident cupiditate placeat fugit dolorum mollitia eaque fuga! Perferendis, libero doloribus ipsum quos rerum consequatur necessitatibus quaerat similique ad facilis error itaque minima voluptatibus in? Itaque quisquam ullam mollitia distinctio repudiandae error dolores possimus, libero cumque? Voluptatem, qui? Consequatur autem perspiciatis ex nesciunt tenetur reprehenderit corporis, tempore nihil, non sed delectus nisi accusantium dicta obcaecati quisquam. Nam nisi, totam ratione facilis, aspernatur odit recusandae dignissimos doloremque magni veritatis reprehenderit deleniti? Eveniet, eligendi! Sapiente ipsam laudantium repellendus cupiditate dicta aspernatur inventore libero, consequatur soluta necessitatibus earum exercitationem. Illum impedit necessitatibus placeat a doloremque architecto voluptates assumenda distinctio repudiandae dolorum exercitationem, hic dicta atque est voluptatibus facilis vero maxime quaerat eaque iste! Dolores, nobis temporibus? Cum enim non fuga, voluptatibus accusamus hic nulla iusto quia doloribus reprehenderit eos eaque provident ipsam explicabo numquam quis dolorem quo repellat obcaecati quae alias? Quidem rerum at sint, atque esse laudantium minus corporis enim vitae reprehenderit pariatur nisi ducimus maxime labore debitis voluptates, consequuntur unde corrupti voluptas perspiciatis? Ipsum cumque aliquid deleniti provident perferendis magnam. At maiores repellendus debitis illo deserunt veniam temporibus iste fugit. Cupiditate suscipit atque minus sunt nesciunt dolorum quisquam quia harum magni ut nihil cumque quidem velit excepturi maxime in eum, aut id, nam </p>
+            </div>
+        </div>
+    </section>
+
+        <!--Seccion de jutsus y tecnicas-->
+
+    <section id="jutsustecnicas">
+        <div class="container">
+            <h2>JUTSUS Y TÉCNICAS</h2>
+        </div>
+    </section>
+
+        <!--Seccion de aldea y localización-->    
+
+    <section id="aldealocalizacion">
+        <div class="container">
+            <h2>ALDEA Y LOCALIZACIÓN</h2>
+        </div>
+    </section>
+
+            <!--Seccion de curiosidades-->
+
+    <section id="curiosidades">
+        <div class="container">
+            <h2>CURIOSIDADES</h2>
+        </div>
+    </section>
+
+        <!--Seccion de galeria-->
+
+    <section id="galeria">
+        <div class="container">
+            <h2>GALERIA</h2>
+        </div>
+    </section>
+
+        <!--Seccion de about us-->
+
+    <section id="aboutus">
+        <div class="container">
+            <h2>ABOUT US</h2>
+        </div>
+    </section>
+
+
+        <!--Seccion de pie de página-->
+        
+    <footer id="mainfooter">
+        <p>Naruto One Page Web 2024&copy;</p>
+    </footer>
+</body>
+</html>
